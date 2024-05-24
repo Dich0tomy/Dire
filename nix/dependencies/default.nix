a88e470b176f68c9f104a43f96473dd6d5a52c69
b4mbus <danielzaradny@gmail.com>
2024-05-24 16:26:35 +0200

    ref: make ./nix/dependencies a flake-parts module

--- nix/dependencies/default.nix
+++ nix/dependencies/default.nix
@@ -1,4 +1,20 @@
-{pkgs}: {
-  tl-optional = pkgs.callPackage ./tl-optional {};
-  tl-expected = pkgs.callPackage ./tl-expected {};
+{
+  inputs,
+  system,
+  ...
+}: {
+  perSystem = {system, ...}: {
+    _module.args = {
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+
+        overlays = [
+          (_: prev: {
+            tl-optional = prev.callPackage ./tl-optional {};
+            tl-expected = prev.callPackage ./tl-expected {};
+          })
+        ];
+      };
+    };
+  };
 }
