d7e531bfbe0a073442d12d038d542cdfff91b3aa
b4mbus <github@yonderly.addy.io>
2024-06-01 01:17:06 +0200

    ref: don't split the output derivation

--- nix/package/default.nix
+++ nix/package/default.nix
@@ -3,11 +3,8 @@
   rootDir,
   version,
   ...
-}: let
-  package = pkgs.callPackage ./package.nix {
+}: {
+  packages.default = pkgs.callPackage ./package.nix {
     inherit rootDir version;
   };
-in {
-  packages.dev = package.dev;
-  packages.default = package.out;
 }
