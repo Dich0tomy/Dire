fa30404576c0ed537cc483b8d60ddede165346db
b4mbus <github@yonderly.addy.io>
2024-07-03 19:48:43 +0200

    ref(nix): make deps legacyPackages instead of overlay

--- nix/dependencies/default.nix
+++ nix/dependencies/default.nix
@@ -1,16 +1,6 @@
-{inputs, ...}: {
-  perSystem = {system, ...}: {
-    _module.args = {
-      pkgs = import inputs.nixpkgs {
-        inherit system;
-
-        overlays = [
-          (_: prev: {
-            tl-optional = prev.callPackage ./tl-optional {};
-            tl-expected = prev.callPackage ./tl-expected {};
-          })
-        ];
-      };
-    };
-  };
+{pkgs, ...}: {
+	legacyPackages = {
+		tl-optional = pkgs.callPackage ./tl-optional {};
+		tl-expected = pkgs.callPackage ./tl-expected {};
+	};
 }
