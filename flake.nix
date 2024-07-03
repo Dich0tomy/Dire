552c7df67fb8112bf65d980a34768b04d9603765
b4mbus <github@yonderly.addy.io>
2024-07-03 19:48:43 +0200

    ref(nix): make deps legacyPackages instead of overlay

--- flake.nix
+++ flake.nix
@@ -5,16 +5,17 @@
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
 
-
       imports = [
-        ./nix/dependencies
         ./nix/package
       ];
 
       perSystem = {pkgs, ...}: {
         formatter = pkgs.alejandra;
 
-        imports = [./nix/shell];
+        imports = [
+					./nix/dependencies
+        	./nix/shell
+        ];
       };
     };
 
--- nix/package/default.nix
+++ nix/package/default.nix
@@ -2,12 +2,13 @@
   perSystem = {
     pkgs,
     lib,
+    config,
     ...
   }: let
     version = lib.strings.fileContents "${self}/VERSION";
   in {
-    packages.default = pkgs.callPackage ./package.nix {
+    packages.default = pkgs.callPackage ./package.nix ({
       inherit self version;
-    };
+    } // config.legacyPackages);
   };
 }
