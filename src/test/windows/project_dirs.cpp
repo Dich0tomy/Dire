f8bc5d090fe561721a5a85d092ba25e81e334b03
b4mbus <github@yonderly.addy.io>
2025-02-22 23:41:22 +0100

    test: adjust windows project tests

--- src/test/windows/project_dirs.cpp
+++ src/test/windows/project_dirs.cpp
@@ -8,9 +8,9 @@ TEST_CASE("name() normalizes name for platform", "[project_dirs]")
 {
 	using dire::Path;
 
-	REQUIRE(static_cast<Path>(dire::name("baz", "bar", "foo")) == "baz\\foo");
-	REQUIRE(static_cast<Path>(dire::name(" F o o", "foo", "     BaZ  ")) == "F o o\\BaZ");
-	REQUIRE(static_cast<Path>(dire::name("domain", "x", "     baz -az ")) == "domain\\baz -az");
+	REQUIRE(static_cast<Path>(dire::name("baz", "bar", "foo")) == "bar\\foo");
+	REQUIRE(static_cast<Path>(dire::name("foo", " F o o", "     BaZ  ")) == "F o o\\BaZ");
+	REQUIRE(static_cast<Path>(dire::name("domain", "x", "     baz -az ")) == "x\\baz -az");
 }
 
 TEST_CASE("name() respects IReallyWantMyOwnPlatformProjectName", "[project_dirs]")
@@ -24,12 +24,12 @@ TEST_CASE("Dir functions respect name", "[project_dirs]")
 {
 	auto name = dire::name("domain", "org", "  a _p - p ");
 
-	REQUIRE(dire::config_dir(name)->string().ends_with("domain\\a _p - p\\config"));
-	REQUIRE(dire::data_dir(name)->string().ends_with("domain\\a _p - p\\data"));
-	REQUIRE(dire::cache_dir(name)->string().ends_with("domain\\a _p - p\\cache"));
-	REQUIRE(dire::data_local_dir(name)->string().ends_with("domain\\a _p - p\\data"));
-	REQUIRE(dire::config_local_dir(name)->string().ends_with("domain\\a _p - p\\config"));
-	REQUIRE(dire::preference_dir(name)->string().ends_with("domain\\a _p - p\\config"));
+	REQUIRE(dire::config_dir(name)->string().ends_with("org\\a _p - p\\config"));
+	REQUIRE(dire::data_dir(name)->string().ends_with("org\\a _p - p\\data"));
+	REQUIRE(dire::cache_dir(name)->string().ends_with("org\\a _p - p\\cache"));
+	REQUIRE(dire::data_local_dir(name)->string().ends_with("org\\a _p - p\\data"));
+	REQUIRE(dire::config_local_dir(name)->string().ends_with("org\\a _p - p\\config"));
+	REQUIRE(dire::preference_dir(name)->string().ends_with("org\\a _p - p\\config"));
 
 	REQUIRE(not dire::runtime_dir(name));
 	REQUIRE(not dire::state_dir(name));
